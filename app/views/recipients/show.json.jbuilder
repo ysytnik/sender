f06bbecc8a802f7d3095317a5092167e0632fc8e
ysytnik <yevhen.sytnik@gmail.com>
2016-06-03 01:25:03 +0200

    Start

---
+++ app/views/recipients/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @recipient, :id, :name, :email, :created_at, :updated_at
---
+++ db/migrate/20160602231708_create_recipients.rb
@@ -0,0 +1,10 @@
+class CreateRecipients < ActiveRecord::Migration
+  def change
+    create_table :recipients do |t|
+      t.string :name
+      t.string :email
+
+      t.timestamps null: false
+    end
+  end
+end
